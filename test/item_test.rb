79646fea2af395f4061a1a963e480ca766f46581
Benmuiruri <ben.muiruri13@gmail.com>
2023-01-11 07:37:04 +0300

    Add test for Item class and update form minimum value

--- test/item_test.rb
+++ test/item_test.rb
@@ -3,5 +3,13 @@ require_relative 'test_helper'
 class ItemTest < Minitest::Test
   include Rack::Test::Methods
 
-  
+  def test_price_per_gram
+    item = Item.new(10, 2)
+    assert_equal 5.0, item.price_per_gram
+  end
+
+  def test_price_per_gram_with_zero_weight
+    item = Item.new(10, 0)
+    assert_raises(RuntimeError) { item.price_per_gram }
+  end
 end
